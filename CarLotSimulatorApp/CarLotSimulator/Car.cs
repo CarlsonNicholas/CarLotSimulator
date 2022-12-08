575a45fe78f3ea1eabffc510907789f0f3dddc71
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-08 11:53:43 -0500

    Main Program Complete

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public Car()
+        {
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string IsDriveable { get; set; }
+
+        public int Year;
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"The engine goes {EngineNoise}"); 
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"The Honk goes {HonkNoise}");
+        }
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -9,7 +9,7 @@ namespace CarLotSimulator
             //TODO
 
             //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable - DONE!
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
@@ -17,6 +17,73 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+            Car car_1 = new Car();
+            Car car_2 = new Car();
+            Car car_3 = new Car();
+
+            // 2004 Ford Mustang - Car 1
+
+            Console.WriteLine("Car 1!");
+
+            car_1.Make = "Ford ";
+            car_1.Model = "Mustang ";
+            car_1.Year = 2004 ;
+            car_1.EngineNoise = "Vroooom! ";
+            car_1.HonkNoise = "honk honk ";
+            car_1.IsDriveable = "Yes ";
+
+            Console.WriteLine(car_1.Make);
+            Console.WriteLine(car_1.Model);
+            Console.WriteLine(car_1.Year);
+            Console.WriteLine($"Is the car driveable: {car_1.IsDriveable}");
+           
+            car_1.MakeEngineNoise();
+            car_1.MakeHonkNoise();
+
+            Console.WriteLine();
+
+            // 2002 Honda Civic - Car 2
+           
+            Console.WriteLine("Car 2!");
+
+            car_2.Make = "Honda ";
+            car_2.Model = "Civic ";
+            car_2.Year = 2004 ;
+            car_2.EngineNoise = "vro ";
+            car_2.HonkNoise = "beep ";
+            car_2.IsDriveable = "Kind of. ";
+
+            Console.WriteLine(car_2.Make);
+            Console.WriteLine(car_2.Model);
+            Console.WriteLine(car_2.Year);
+            Console.WriteLine($"Is the car driveable: {car_2.IsDriveable}");
+
+            car_2.MakeEngineNoise();
+            car_2.MakeHonkNoise();
+
+            Console.WriteLine();
+
+            // 2023 Chrysler 300 - Car 3
+
+            Console.WriteLine("Car 3!");
+
+            car_3.Make = "Chrysler ";
+            car_3.Model = "300 ";
+            car_3.Year = 2023 ;
+            car_3.EngineNoise = "BROOM! BROOM! ";
+            car_3.HonkNoise = "Ba-beep! ";
+            car_3.IsDriveable = "Yes ";
+
+            Console.WriteLine(car_3.Make);
+            Console.WriteLine(car_3.Model);
+            Console.WriteLine(car_3.Year);
+            Console.WriteLine($"Is the car driveable: {car_3.IsDriveable}");
+
+            car_3.MakeEngineNoise();
+            car_3.MakeHonkNoise();
+
+            Console.WriteLine();
 
             //*************BONUS*************//
 
